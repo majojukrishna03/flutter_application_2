6c1013839854ce0f91f87244e68ad180b3422ac4
sridivya <sridivya1508@gmail.com>
2024-03-05 16:19:22 +0530

    added comment

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() {
 
 abstract class CalculatorOperations {
   double add(double num1, double num2);
-  //subtract class
   double subtract(double num1, double num2);
   double multiply(double num1, double num2);
   double divide(double num1, double num2);
